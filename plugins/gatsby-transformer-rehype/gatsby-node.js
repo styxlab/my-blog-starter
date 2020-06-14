50c0bec0d7b451b317ff689a4b9606b6fd92459c
cws <cws@home.fritz.box>
2020-06-14 16:10:34 +0200

    latest

--- plugins/gatsby-transformer-rehype/gatsby-node.js
+++ plugins/gatsby-transformer-rehype/gatsby-node.js
@@ -1,4 +1,4 @@
 "use strict";
 
-exports.createSchemaCustomization = require(`./create-schema-customization`);
-exports.onCreateNode = require(`./on-node-create`); //exports.setFieldsOnGraphQLNodeType = require(`./extend-node-type`)
+exports.createSchemaCustomization = require(`./create-schema-customization`); //exports.onCreateNode = require(`./on-node-create`)
+//exports.setFieldsOnGraphQLNodeType = require(`./extend-node-type`)
